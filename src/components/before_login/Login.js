7f4aa139b7625e414b0a6ece9bf491e99bc6e2a6
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-01-07 18:13:41 -0800

    new

--- src/components/before_login/Login.js
+++ src/components/before_login/Login.js
@@ -77,7 +77,7 @@ const Login = () => {
                 console.log(res.data)
                 localStorage.setItem('token', res.data.token)
                 localStorage.setItem('id', res.data.user_id)
-                window.location.href = `https://elastic-gates-3ba026.netlify.com/login/home/homepage`
+                window.location.href = `https://elastic-gates-3ba026.netlify.com/home/homepage`
                 // console.log(localStorage.getItem)
              
             })
